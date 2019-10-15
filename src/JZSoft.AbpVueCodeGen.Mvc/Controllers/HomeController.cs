639a2ba21666629e924ada9e8187660c0ebbe40f
hyzx8 <yanzhong.han@jizhousoft.com>
2019-10-15 14:55:52 +0800

    add filter

--- src/JZSoft.AbpVueCodeGen.Mvc/Controllers/HomeController.cs
+++ src/JZSoft.AbpVueCodeGen.Mvc/Controllers/HomeController.cs
@@ -4,7 +4,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using JZSoft.AbpVueCodeGen.Mvc.Models;  
+using JZSoft.AbpVueCodeGen.Mvc.Models;
+using Newtonsoft.Json.Linq;
 
 namespace JZSoft.AbpVueCodeGen.Mvc.Controllers
 {
@@ -15,16 +16,23 @@ namespace JZSoft.AbpVueCodeGen.Mvc.Controllers
         public IActionResult Index()
         { 
             return View();
-        }
-
-
+        } 
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
-
+        public IActionResult GenCode(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+            {
+                return View();
+            }
+            JObject jObject = JObject.Parse(json);
+            Response.ContentType = "text/plian;charset=utf-8";
+            return View(jObject);
+        }
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
